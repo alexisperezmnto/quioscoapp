fd68a848c30ff2057034497391c261d71644ae6d
prezmon <alexisperezmnto@gmail.com>
2022-07-17 17:26:24 -0500

    Modified seed categorias

--- prisma/data/categorias.ts
+++ prisma/data/categorias.ts
@@ -1,25 +1,31 @@
 const categorias = [
     {
+        id:1,
         icono: "cafe",
         nombre: "Café"
       },
       {
+        id:2,
         icono: "hamburguesa",
         nombre: "Hamburguesas"
       },
       {
+        id:3,
         icono: "pizza",
         nombre: "Pizzas"
       },
       {
+        id:4,
         icono: "dona",
         nombre: "Donas"
       },
       {
+        id:5,
         icono: "pastel",
         nombre: "Pasteles"
       },
       {
+        id:6,
         icono: "galletas",
         nombre: "Galletas"
       }
